9ef3f24627fd254bce4650e8c930a26531ebc5cc
BisPeter <peter.vadle@gmail.com>
2020-11-10 14:52:06 +0100

    insight build

--- Simple_Web_App/index.js
+++ Simple_Web_App/index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const appInsights = require('applicationinsights');
+
+
+appInsights.setup('bd018e96-762a-4bf9-af8f-9877c74a0c7f').start();
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
